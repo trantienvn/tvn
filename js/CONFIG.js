f27163b938bb63e3e2dab314204f5ec2c6a8880b
trantienvn <97656011+trantienvn@users.noreply.github.com>
2022-08-17 15:34:42 +0700

    Add files via upload

---
+++ js/CONFIG.js
@@ -0,0 +1,58 @@
+const __DATA__ = {
+    
+    'cover-image': 't.jpg',
+    'avatar-image': 'avt.png',
+    'name': 'Trần Tiến',
+    'skills': [
+        'Thích Movie',
+        'Marvel',
+    ],
+    'bio-storys': [{
+        'image': 'ad.jpg',
+        'content': 'Giới thiệu',
+        'author': 'Chào Bạn 👋,tui sống tại TP Phổ Yên',
+    }, ],
+    'links': [{
+            'image': 'facebook.jpg',
+            'title': 'Facebook',
+            'show-value': 'Trần Tiến',
+            'value': 'https://www.facebook.com/trantienpy/',
+            'type': 0,
+        },
+		{
+            'image': 'mess.jpg',
+            'title': 'Messenger',
+            'show-value': 'Trần Tiến',
+            'value': 'https://m.me/trantienpy',
+            'type': 0,
+        },
+        {
+            'image': 'tele.png',
+            'title': 'Telegram',
+            'show-value': 'Trần Tiến',
+            'value': 'https://t.me/trantienpy',
+            'type': 0,
+        },
+        {
+            'image': 'gmail.jpg',
+            'title': 'Gmail',
+            'show-value': 'tientn2005@gmail.com',
+            'value': 'tientn2005@gmail.com',
+            'type': 1,
+        },
+        {
+            'image': 'momo.jpg',
+            'title': 'Momo',
+            'show-value': 'Trần Văn Tiến',
+            'value': 'https://me.momo.vn/trantienvn',
+            'type': 0,
+        },
+		{
+            'image': 'mb.png',
+            'title': 'MB Bank',
+            'show-value': 'Trần Văn Tiến',
+            'value': '0382652361',
+            'type': 1,
+        },
+    ]
+};
