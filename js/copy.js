a6d9d0b74923932aae2081b69ff6d283cdb1b742
TranTien <tientn2005@gmail.com>
2024-06-29 09:36:30 +0700

    remake

--- js/copy.js
+++ js/copy.js
@@ -9,31 +9,7 @@ class TranTienTN extends Error{
         onDToolopened();
     }
 }
-// Kiểm tra xem trang web đã được mở trong tab khác chưa
-if (localStorage.getItem('isPageOpen') === null) {
-    // Trang web chưa được mở trong tab nào cả
-    localStorage.setItem('isPageOpen', 'true');
-  } else {
-    // Trang web đã được mở trong tab khác
-    alert('Trang web đã được mở trong một tab khác. Đang đóng tab cũ...');
-    // Đóng tab cũ
-    window.top.close();
-  }
-  
-  // Lắng nghe sự kiện khi tab mới được mở
-  window.addEventListener('storage', function(event) {
-    if (event.key === 'isPageOpen' && event.newValue === null) {
-      // Tab khác đã đóng trang web
-      alert('Trang web đã được đóng trong tab khác.');
-    }
-  });
-  
-  // Lắng nghe sự kiện trước khi tab đóng
-  window.addEventListener('beforeunload', function(event) {
-    // Ghi dữ liệu vào localStorage trước khi tab đóng
-    localStorage.removeItem('isPageOpen');
-  });
-  
+
 function getKey(e) {
     var key = e.keyCode;
     console.log(key);
